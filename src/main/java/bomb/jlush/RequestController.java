6960636f0f84694be5bc3b5b0c28c4de336b12b1
Unknown <v-JoshuaStewart@smarttech.com>
2019-03-06 09:30:31 +0200

    comit leafies

---
+++ src/main/java/bomb/jlush/RequestController.java
@@ -0,0 +1,50 @@
+package bomb.jlush;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class RequestController {
+
+    BlogMetadata blogMetadata = BlogMetadata.getInstance();
+
+    @GetMapping("/blog")
+    public List<Blog> index(){
+        return blogMetadata.fetchBlogs();
+    }
+
+    @GetMapping("/blog/{id}")
+    public Blog show(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        return blogMetadata.getBlogById(blogId);
+    }
+
+    @PostMapping("/blog/search")
+    public List<Blog> search(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return blogMetadata.searchBlogs(searchTerm);
+    }
+
+    @PostMapping("/blog")
+    public Blog create(@RequestBody Map<String, String> body){
+        int id = Integer.parseInt(body.get("id"));
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogMetadata.createBlog(id, title, content);
+    }
+
+    @PutMapping("/blog/{id}")
+    public Blog update(@PathVariable String id, @RequestBody Map<String, String> body){
+        int blogId = Integer.parseInt(id);
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogMetadata.updateBlog(blogId, title, content);
+    }
+
+    @DeleteMapping("blog/{id}")
+    public boolean delete(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        return blogMetadata.delete(blogId);
+    }
+}
